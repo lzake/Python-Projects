eb826a0512147aea6800553d86a1adf130f63244
Zachariah Lowe <lowezake@gmail.com>
2017-08-09 19:57:10 -0600

    examples push

--- negotiate.py
+++ negotiate.py
@@ -6,8 +6,8 @@ if salary <= 100000:
    local =  input('Enter location of offered job: ')
    if local != ('Colorado' or 'Denver' or 'Co' or 'Denver, Co'):
       if salary <= 110000:
-          increase = input('Can you increase the offer?')
-          if increase == ('yes' or 'yea' or 'yeah'):
+          increase = input('Can you increase the offer? ')
+          if increase == ('yes' or 'yea' or 'yeah' or 'Yes' or 'Yea' or 'Yeah'):
               howMuch = int(input('How much?'))
               if howMuch >= 105000:
                   print('When can I start?')
